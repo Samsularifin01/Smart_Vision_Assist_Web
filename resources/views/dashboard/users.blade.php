5ab7fe3ebea3ad1e17d2abaf026f84791cc25830
Samsularifin01 <158025491+Samsularifin01@users.noreply.github.com>
2026-04-22 21:06:45 +0700

    add link css in user.blade.php

--- resources/views/dashboard/users.blade.php
+++ resources/views/dashboard/users.blade.php
@@ -5,6 +5,9 @@
     <title>Users</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+    <link href="/css/dashboard.css" rel="stylesheet">
+    <link href="/css/sidebar.css" rel="stylesheet">
 </head>
 
 <body>
